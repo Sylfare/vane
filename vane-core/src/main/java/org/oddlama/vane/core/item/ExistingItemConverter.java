8f0e7c0e58c3cb679c5f7b73342107d34decf891
Sylfare <12037446+Sylfare@users.noreply.github.com>
2025-07-07 23:13:31 +0200

    fix: build errors + bug with resource pack UUID

--- vane-core/src/main/java/org/oddlama/vane/core/item/ExistingItemConverter.java
+++ vane-core/src/main/java/org/oddlama/vane/core/item/ExistingItemConverter.java
@@ -15,178 +15,127 @@ import org.oddlama.vane.core.item.api.CustomItem;
 import org.oddlama.vane.core.module.Context;
 
 public class ExistingItemConverter extends Listener<Core> {
+	public ExistingItemConverter(final Context<Core> context) {
+		super(context.namespace("existing_item_converter"));
+	}
 
-    public ExistingItemConverter(final Context<Core> context) {
-        super(context.namespace("existing_item_converter"));
-    }
-
-    private CustomItem from_old_item(final ItemStack item_stack) {
-        final var meta = item_stack.getItemMeta();
-        if (meta == null || !meta.hasCustomModelData()) {
-            return null;
-        }
-
-        // If lookups fail, we return null and nothing will be done.
-        String new_item_key = null;
-		if (meta.getCustomModelDataComponent().getFloats().isEmpty()) {
+	private CustomItem from_old_item(final ItemStack item_stack) {
+		final var modelDataList = item_stack.getItemMeta().getCustomModelDataComponent().getFloats();
+		if (modelDataList.isEmpty() || modelDataList.getFirst() == null) {
 			return null;
 		}
-        switch (meta.getCustomModelData()) {
-            case 7758190:
-                new_item_key = "vane_trifles:wooden_sickle";
-                break;
-            case 7758191:
-                new_item_key = "vane_trifles:stone_sickle";
-                break;
-            case 7758192:
-                new_item_key = "vane_trifles:iron_sickle";
-                break;
-            case 7758193:
-                new_item_key = "vane_trifles:golden_sickle";
-                break;
-            case 7758194:
-                new_item_key = "vane_trifles:diamond_sickle";
-                break;
-            case 7758195:
-                new_item_key = "vane_trifles:netherite_sickle";
-                break;
-            case 7758254: // fallthrough
-            case 7758255: // fallthrough
-            case 7758256: // fallthrough
-            case 7758257: // fallthrough
-            case 7758258: // fallthrough
-            case 7758259:
-                new_item_key = "vane_trifles:file";
-                break;
-            case 7758318:
-                new_item_key = "vane_trifles:empty_xp_bottle";
-                break;
-            case 7758382:
-                new_item_key = "vane_trifles:small_xp_bottle";
-                break;
-            case 7758383:
-                new_item_key = "vane_trifles:medium_xp_bottle";
-                break;
-            case 7758384:
-                new_item_key = "vane_trifles:large_xp_bottle";
-                break;
-            case 7758446:
-                new_item_key = "vane_trifles:home_scroll";
-                break;
-            case 7758510:
-                new_item_key = "vane_trifles:unstable_scroll";
-                break;
-            case 7758574:
-                new_item_key = "vane_trifles:reinforced_elytra";
-                break;
-            case 7823726:
-                new_item_key = "vane_enchantments:ancient_tome";
-                break;
-            case 7823727:
-                new_item_key = "vane_enchantments:enchanted_ancient_tome";
-                break;
-            case 7823790:
-                new_item_key = "vane_enchantments:ancient_tome_of_knowledge";
-                break;
-            case 7823791:
-                new_item_key = "vane_enchantments:enchanted_ancient_tome_of_knowledge";
-                break;
-            case 7823854:
-                new_item_key = "vane_enchantments:ancient_tome_of_the_gods";
-                break;
-            case 7823855:
-                new_item_key = "vane_enchantments:enchanted_ancient_tome_of_the_gods";
-                break;
-        }
 
-        if (new_item_key == null) {
-            return null;
-        }
-        return get_module().item_registry().get(NamespacedKey.fromString(new_item_key));
-    }
+		// If lookups fail, we return null and nothing will be done.
+		String new_item_key = switch (modelDataList.getFirst().intValue()) {
+            case 7758190 -> "vane_trifles:wooden_sickle";
+            case 7758191 -> "vane_trifles:stone_sickle";
+            case 7758192 -> "vane_trifles:iron_sickle";
+            case 7758193 -> "vane_trifles:golden_sickle";
+            case 7758194 -> "vane_trifles:diamond_sickle";
+            case 7758195 -> "vane_trifles:netherite_sickle";
+            case 7758254,7758255,7758256,7758257,7758258,7758259 -> "vane_trifles:file";
+            case 7758318 -> "vane_trifles:empty_xp_bottle";
+            case 7758382 -> "vane_trifles:small_xp_bottle";
+            case 7758383 -> "vane_trifles:medium_xp_bottle";
+            case 7758384 -> "vane_trifles:large_xp_bottle";
+            case 7758446 -> "vane_trifles:home_scroll";
+            case 7758510 -> "vane_trifles:unstable_scroll";
+            case 7758574 -> "vane_trifles:reinforced_elytra";
+            case 7823726 -> "vane_enchantments:ancient_tome";
+            case 7823727 -> "vane_enchantments:enchanted_ancient_tome";
+            case 7823790 -> "vane_enchantments:ancient_tome_of_knowledge";
+            case 7823791 -> "vane_enchantments:enchanted_ancient_tome_of_knowledge";
+            case 7823854 -> "vane_enchantments:ancient_tome_of_the_gods";
+            case 7823855 -> "vane_enchantments:enchanted_ancient_tome_of_the_gods";
+            default->null;
+        };
 
-    private void process_inventory(@NotNull Inventory inventory) {
-        final var contents = inventory.getContents();
-        int changed = 0;
+		if (new_item_key == null) {
+			return null;
+		}
+		return get_module().item_registry().get(NamespacedKey.fromString(new_item_key));
+	}
 
-        for (int i = 0; i < contents.length; ++i) {
-            final var is = contents[i];
-            if (is == null || !is.hasItemMeta()) {
-                continue;
-            }
+	private void process_inventory(@NotNull Inventory inventory) {
+		final var contents = inventory.getContents();
+		int changed = 0;
 
-            final var custom_item = get_module().item_registry().get(is);
-            if (custom_item == null) {
-                // Determine if the item stack should be converted to a custom item from a legacy
-                // definition
-                final var convert_to_custom_item = from_old_item(is);
-                if (convert_to_custom_item == null) {
-                    continue;
-                }
+		for (int i = 0; i < contents.length; ++i) {
+			final var is = contents[i];
+			if (is == null || !is.hasItemMeta()) {
+				continue;
+			}
 
-                contents[i] = convert_to_custom_item.convertExistingStack(is);
-                contents[i].editMeta(meta -> meta.itemName(convert_to_custom_item.displayName()));
-                get_module().enchantment_manager.update_enchanted_item(contents[i]);
-                get_module().log.info("Converted legacy item to " + convert_to_custom_item.key());
-                ++changed;
-                continue;
-            }
+			final var custom_item = get_module().item_registry().get(is);
+			if (custom_item == null) {
+				// Determine if the item stack should be converted to a custom item from a legacy
+				// definition
+				final var convert_to_custom_item = from_old_item(is);
+				if (convert_to_custom_item == null) {
+					continue;
+				}
 
-            // Remove obsolete custom items
-            if (get_module().item_registry().shouldRemove(custom_item.key())) {
-                contents[i] = null;
-                get_module().log.info("Removed obsolete item " + custom_item.key());
-                ++changed;
-                continue;
-            }
+				contents[i] = convert_to_custom_item.convertExistingStack(is);
+				contents[i].editMeta(meta -> meta.itemName(convert_to_custom_item.displayName()));
+				get_module().enchantment_manager.update_enchanted_item(contents[i]);
+				get_module().log.info("Converted legacy item to " + convert_to_custom_item.key());
+				++changed;
+				continue;
+			}
 
-            // Update custom items to a new version, or if another detectable property changed.
-            final var key_and_version = CustomItemHelper.customItemTagsFromItemStack(is);
-            final var meta = is.getItemMeta();
-            if (
-                meta.getCustomModelData() != custom_item.customModelData() ||
-                is.getType() != custom_item.baseMaterial() ||
-                key_and_version.getRight() != custom_item.version()
-            ) {
-                // Also includes durability max update.
-                contents[i] = custom_item.convertExistingStack(is);
-                get_module().log.info("Updated item " + custom_item.key());
-                ++changed;
-                continue;
-            }
+			// Remove obsolete custom items
+			if (get_module().item_registry().shouldRemove(custom_item.key())) {
+				contents[i] = null;
+				get_module().log.info("Removed obsolete item " + custom_item.key());
+				++changed;
+				continue;
+			}
 
-            // Update maximum durability on existing items if changed.
-            Damageable damageableMeta = (Damageable) contents[i].getItemMeta();
-            int max_damage = damageableMeta.hasMaxDamage()
-                ? damageableMeta.getMaxDamage()
-                : contents[i].getType().getMaxDurability();
-            int correct_max_damage = custom_item.durability() == 0
-                ? contents[i].getType().getMaxDurability()
-                : custom_item.durability();
-            if (
-                max_damage != correct_max_damage ||
-                meta.getPersistentDataContainer().has(DurabilityManager.ITEM_DURABILITY_DAMAGE)
-            ) {
-                get_module().log.info("Updated item durability " + custom_item.key());
-                DurabilityManager.update_damage(custom_item, contents[i]);
-                ++changed;
-                continue;
-            }
-        }
+			// Update custom items to a new version, or if another detectable property changed.
+			final var key_and_version = CustomItemHelper.customItemTagsFromItemStack(is);
+			final var meta = is.getItemMeta();
+			if (
+			    meta.getCustomModelData() != custom_item.customModelData() ||
+			    is.getType() != custom_item.baseMaterial() ||
+			    key_and_version.getRight() != custom_item.version()) {
+				// Also includes durability max update.
+				contents[i] = custom_item.convertExistingStack(is);
+				get_module().log.info("Updated item " + custom_item.key());
+				++changed;
+				continue;
+			}
 
-        if (changed > 0) {
-            inventory.setContents(contents);
-        }
-    }
+			// Update maximum durability on existing items if changed.
+			Damageable damageableMeta = (Damageable)contents[i].getItemMeta();
+			int max_damage = damageableMeta.hasMaxDamage()
+			                     ? damageableMeta.getMaxDamage()
+			                     : contents[i].getType().getMaxDurability();
+			int correct_max_damage = custom_item.durability() == 0
+			                             ? contents[i].getType().getMaxDurability()
+			                             : custom_item.durability();
+			if (
+			    max_damage != correct_max_damage ||
+			    meta.getPersistentDataContainer().has(DurabilityManager.ITEM_DURABILITY_DAMAGE)) {
+				get_module().log.info("Updated item durability " + custom_item.key());
+				DurabilityManager.update_damage(custom_item, contents[i]);
+				++changed;
+				continue;
+			}
+		}
 
-    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
-    public void on_player_join(final PlayerJoinEvent event) {
-        process_inventory(event.getPlayer().getInventory());
-    }
+		if (changed > 0) {
+			inventory.setContents(contents);
+		}
+	}
 
-    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
-    public void on_inventory_open(final InventoryOpenEvent event) {
-        // Catches enderchests, and inventories by other plugins
-        process_inventory(event.getInventory());
-    }
+	@EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+	public void on_player_join(final PlayerJoinEvent event) {
+		process_inventory(event.getPlayer().getInventory());
+	}
+
+	@EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+	public void on_inventory_open(final InventoryOpenEvent event) {
+		// Catches enderchests, and inventories by other plugins
+		process_inventory(event.getInventory());
+	}
 }
--- vane-portals/src/main/java/org/oddlama/vane/portals/entity/FloatingItem.java
+++ vane-portals/src/main/java/org/oddlama/vane/portals/entity/FloatingItem.java
@@ -7,91 +7,93 @@ import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.storage.ValueInput;
+import net.minecraft.world.level.storage.ValueOutput;
 import org.bukkit.Location;
 import org.bukkit.World;
+import org.jetbrains.annotations.NotNull;
 
 public class FloatingItem extends ItemEntity {
+	public FloatingItem(final Location location) {
+		this(location.getWorld(), location.getX(), location.getY(), location.getZ());
+	}
 
-    public FloatingItem(final Location location) {
-        this(location.getWorld(), location.getX(), location.getY(), location.getZ());
-    }
+	public FloatingItem(final World world, double x, double y, double z) {
+		this(EntityType.ITEM, world_handle(world));
+		setPos(x, y, z);
+	}
 
-    public FloatingItem(final World world, double x, double y, double z) {
-        this(EntityType.ITEM, world_handle(world));
-        setPos(x, y, z);
-    }
+	public FloatingItem(EntityType<?> entitytypes, Level world) {
+		super((EntityType<? extends ItemEntity>)entitytypes, world);
+		setSilent(true);
+		setInvulnerable(true);
+		setNoGravity(true);
+		// setSneaking(true); // Names would then only visible on direct line of sight BUT much
+		// darker and offset by -0.5 in y direction
+		setNeverPickUp();
+		setUnlimitedLifetime();
+		persist = false;
+		noPhysics = true;
+	}
 
-    public FloatingItem(EntityType<?> entitytypes, Level world) {
-        super((EntityType<? extends ItemEntity>)entitytypes, world);
-        setSilent(true);
-        setInvulnerable(true);
-        setNoGravity(true);
-        // setSneaking(true); // Names would then only visible on direct line of sight BUT much
-        // darker and offset by -0.5 in y direction
-        setNeverPickUp();
-        setUnlimitedLifetime();
-        persist = false;
-        noPhysics = true;
-    }
+	@Override
+	public boolean isAlive() {
+		// Required to efficiently prevent hoppers and hopper minecarts from picking this up
+		return false;
+	}
 
-    @Override
-    public boolean isAlive() {
-        // Required to efficiently prevent hoppers and hopper minecarts from picking this up
-        return false;
-    }
+	@Override
+	public boolean isAttackable() {
+		return false;
+	}
 
-    @Override
-    public boolean isAttackable() {
-        return false;
-    }
+	@Override
+	public boolean isCollidable(boolean ignoreClimbing) {
+		return false;
+	}
 
-    @Override
-    public boolean isCollidable(boolean ignoreClimbing) {
-        return false;
-    }
+	@Override
+	public boolean isInvisible() {
+		return true;
+	}
 
-    @Override
-    public boolean isInvisible() {
-        return true;
-    }
+	@Override
+	public boolean fireImmune() {
+		return true;
+	}
 
-    @Override
-    public boolean fireImmune() {
-        return true;
-    }
+	@Override
+	public void tick() {}
 
-    @Override
-    public void tick() {}
+	@Override
+	public void inactiveTick() {}
 
-    @Override
-    public void inactiveTick() {}
+	// Don't save or load
+	@Override
+	public void readAdditionalSaveData(@NotNull ValueInput output) {}
 
-    // Don't save or load
-    @Override
-    public void readAdditionalSaveData(CompoundTag nbt) {}
+	@Override
+	protected void addAdditionalSaveData(@NotNull ValueOutput output) {}
 
-    @Override
-    public void addAdditionalSaveData(CompoundTag nbt) {}
+	@Override
+	public boolean save(@NotNull ValueOutput output) {
+		return false;
+	}
 
-    @Override
-    public boolean save(CompoundTag nbt) {
-        return false;
-    }
+	@Override
+	public void saveWithoutId(@NotNull ValueOutput output) {}
 
-    @Override
-    public CompoundTag saveWithoutId(CompoundTag nbt) {
-        return nbt;
-    }
+	@Override
+	public void load(@NotNull ValueInput output) {}
 
-    @Override
-    public void load(CompoundTag nbt) {}
-
-    @Override
-    public void setItem(ItemStack itemStack) {
-        super.setItem(itemStack);
-        if (itemStack.getHoverName().toFlatList().size() > 0) {
+	@Override
+	public void setItem(@NotNull ItemStack itemStack) {
+		super.setItem(itemStack);
+		if (itemStack.getHoverName().toFlatList().isEmpty()) {
+            setCustomNameVisible(false);
+		} else {
             setCustomNameVisible(true);
             setCustomName(itemStack.getHoverName());
-        } else setCustomNameVisible(false);
-    }
+        }
+	}
 }
